bf9a23dc957fb00db96d01cce594c2417bd84505
handrin <handrin@uwo.ca>
2016-05-26 17:57:12 -0400

    Add variables and methods to order class

--- app/src/main/java/com/rhinoactive/pizzadeliveryapp/model/Order.java
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/model/Order.java
@@ -1,11 +1,38 @@
 package com.rhinoactive.pizzadeliveryapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Matt on 5/25/16.
  */
 public class Order {
 
-    public int orderId;
-    //...
+    private int orderId;
+    private Customer customer;
+    private Address customerAddress;
+    private List<Pizza> pizzas;
 
+    public Order(int orderId, Customer customer, Address customerAddress, ArrayList<Pizza> pizzas){
+        this.orderId = orderId;
+        this.customer = customer;
+        this.customerAddress = customerAddress;
+        this.pizzas = pizzas;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Address getCustomerAddress() {
+        return customerAddress;
+    }
+
+    public List<Pizza> getPizzas() {
+        return pizzas;
+    }
 }
