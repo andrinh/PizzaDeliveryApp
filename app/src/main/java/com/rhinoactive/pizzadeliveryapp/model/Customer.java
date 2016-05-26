1e7e12443d26b0cf8cb35defe77cea421117b9c5
handrin <handrin@uwo.ca>
2016-05-26 15:59:57 -0400

    Create customer class

---
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/model/Customer.java
@@ -0,0 +1,28 @@
+package com.rhinoactive.pizzadeliveryapp.model;
+
+/**
+ * Created by Huntur on 26/05/2016.
+ */
+public class Customer {
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public Customer(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
