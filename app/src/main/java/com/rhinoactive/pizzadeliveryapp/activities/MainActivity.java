fb79e7621d4701507b27164514308dd12545e76f
handrin <handrin@uwo.ca>
2016-05-26 23:48:06 -0400

    Initialize buttons and editTexts in MainActivity controller

--- app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
@@ -2,15 +2,29 @@ package com.rhinoactive.pizzadeliveryapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.rhinoactive.pizzadeliveryapp.R;
 
 public class MainActivity extends AppCompatActivity {
+    private Button submitButton;
+    private EditText firstName, lastName, email, street, city, postalCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+    }
 
+    private void init(){
+        submitButton = (Button)findViewById(R.id.submitButton);
+        firstName = (EditText)findViewById(R.id.editFirstNameText);
+        lastName = (EditText)findViewById(R.id.editLastNameText);
+        email = (EditText)findViewById(R.id.editEmailText);
+        street = (EditText)findViewById(R.id.editStreetText);
+        city = (EditText)findViewById(R.id.editCityText);
+        postalCode = (EditText)findViewById(R.id.editPostalCodeText);
     }
 }
