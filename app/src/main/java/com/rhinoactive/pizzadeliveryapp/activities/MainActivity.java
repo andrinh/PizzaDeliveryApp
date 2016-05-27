679fb6763a06660d7ad8ade1afd32df56555389e
handrin <handrin@uwo.ca>
2016-05-27 13:23:55 -0400

    Add click listener to submit button

--- app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
@@ -4,18 +4,23 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
 import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 import com.rhinoactive.pizzadeliveryapp.R;
+import com.rhinoactive.pizzadeliveryapp.model.Customer;
 
 public class MainActivity extends AppCompatActivity {
     private Button submitButton;
     private EditText firstName, lastName, email, street, city, postalCode;
+    private Customer customer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        addListenerOnSubmitButton();
     }
 
     private void init(){
@@ -26,5 +31,15 @@ public class MainActivity extends AppCompatActivity {
         street = (EditText)findViewById(R.id.editStreetText);
         city = (EditText)findViewById(R.id.editCityText);
         postalCode = (EditText)findViewById(R.id.editPostalCodeText);
+    }
+
+    private void addListenerOnSubmitButton() {
+        submitButton.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                customer = new Customer(firstName.getText().toString(),
+                        lastName.getText().toString(), email.getText().toString());
+
+            }
+        });
     }
 }
