3e2c4ec81c9cb1fd9631a29b4e6142ecc97ccec4
handrin <handrin@uwo.ca>
2016-05-28 01:09:57 -0400

    Remove pizza containers when user reselects number of pizzas
    -refactor creation of pizza container

--- app/src/main/java/com/rhinoactive/pizzadeliveryapp/utils/OnPizzaNumberItemSelectedListener.java
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/utils/OnPizzaNumberItemSelectedListener.java
@@ -1,33 +1,60 @@
 package com.rhinoactive.pizzadeliveryapp.utils;
 
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Huntur on 27/05/2016.
  */
 public class OnPizzaNumberItemSelectedListener implements OnItemSelectedListener {
     private LinearLayout mainLayout;
+    private LinearLayout.LayoutParams params;
+    private List<LinearLayout> pizzaContainers;
 
     public OnPizzaNumberItemSelectedListener(LinearLayout mainLayout) {
         this.mainLayout = mainLayout;
+        params = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        pizzaContainers = new ArrayList<>();
+
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+        removeExistingPizzaContainers();
         int numberOfPizzas = pos;
-        for (int i = 1; i <= numberOfPizzas; i++) {
+        for (int pizzaNumber = 1; pizzaNumber <= numberOfPizzas; pizzaNumber++) {
+            LinearLayout pizzaContainer = createPizzaContainerLayout(parent);
             TextView pizzaHeader = new TextView(parent.getContext());
-            pizzaHeader.setText(Constants.PIZZA + " " + i);
-            mainLayout.addView(pizzaHeader);
-            mainLayout.addView();
+            pizzaHeader.setText(Constants.PIZZA + " " + pizzaNumber);
+            pizzaContainer.addView(pizzaHeader);
+            pizzaContainers.add(pizzaContainer);
+            mainLayout.addView(pizzaContainer);
         }
     }
 
     @Override
     public void onNothingSelected(AdapterView<?> arg0) {
         // TODO Auto-generated method stub
+    }
+
+    private void removeExistingPizzaContainers(){
+        for (LinearLayout pizzaContainer : pizzaContainers) {
+            pizzaContainer.removeAllViews();
+        }
+        pizzaContainers.removeAll(pizzaContainers);
+    }
+
+    private LinearLayout createPizzaContainerLayout(AdapterView<?> parent){
+        LinearLayout pizzaContainer = new LinearLayout(parent.getContext());
+        pizzaContainer.setOrientation(LinearLayout.VERTICAL);
+        pizzaContainer.setLayoutParams(params);
+        return pizzaContainer;
     }
 }
