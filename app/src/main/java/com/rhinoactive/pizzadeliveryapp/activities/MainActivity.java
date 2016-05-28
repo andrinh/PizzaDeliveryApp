530e7da5e7d63dcedd78a22edae7a79d7c0e6acd
handrin <handrin@uwo.ca>
2016-05-27 20:39:30 -0400

    Create a customer address when submit button is pressed

--- app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
+++ app/src/main/java/com/rhinoactive/pizzadeliveryapp/activities/MainActivity.java
@@ -3,17 +3,21 @@ package com.rhinoactive.pizzadeliveryapp.activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.EditText;
 import android.view.View;
 import android.view.View.OnClickListener;
 
 import com.rhinoactive.pizzadeliveryapp.R;
+import com.rhinoactive.pizzadeliveryapp.model.Address;
 import com.rhinoactive.pizzadeliveryapp.model.Customer;
 
 public class MainActivity extends AppCompatActivity {
     private Button submitButton;
     private EditText firstName, lastName, email, street, city, postalCode;
+    private Spinner province;
     private Customer customer;
+    private Address customerAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
         street = (EditText)findViewById(R.id.editStreetText);
         city = (EditText)findViewById(R.id.editCityText);
         postalCode = (EditText)findViewById(R.id.editPostalCodeText);
+        province = (Spinner)findViewById(R.id.spinnerProvince);
     }
 
     private void addListenerOnSubmitButton() {
@@ -38,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 customer = new Customer(firstName.getText().toString(),
                         lastName.getText().toString(), email.getText().toString());
+                customerAddress = new Address(street.getText().toString(),
+                        city.getText().toString(), String.valueOf(province.getSelectedItem()),
+                        postalCode.getText().toString());
 
             }
         });
